9642b1edb3d2e2bd7aa1bec1ab95d061e814f994
Julien Van Tongerloo <julien.vantongerloo@intuitim.com>
2022-10-10 15:24:19 +0200

    add - server

--- Makefile
+++ Makefile
@@ -4,16 +4,19 @@ CCFLAGS=-D_DEFAULT_SOURCE -D_XOPEN_SOURCE -D_BSD_SOURCE -std=c11 -pedantic -Wvla
 all: server client
 
 server : server.o 
-	$(CC) $(CCFLAGS) -o server server.o
+	$(CC) $(CCFLAGS) -o server server.o utils.o
 
 client : client.o 
-	$(CC) $(CCFLAGS) -o client client.o
+	$(CC) $(CCFLAGS) -o client client.o utils.o
 
-server.o: server.c
+server.o: server.c utils.o
 	$(CC) $(CCFLAGS) -c server.c 
 
-client.o: client.c
+client.o: client.c utils.o
 	$(CC) $(CCFLAGS) -c client.c
+
+utils.o: utils.c utils.h
+	$(CC) $(CCFLAGS) -c utils.c 
 
 clean : 
 	rm -f server client *.o 
--- server.c
+++ server.c
@@ -1,7 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-int main(int argc, char **argv)
+#include "utils.h"
+
+#define BACKLOG 5
+
+volatile sig_atomic_t end;
+
+void endServerHandler(int sig) {
+  end = 1;
+}
+
+int initSocketServer(int port)
 {
+  int sockfd = ssocket();
+    
+  int option = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int));
+  
+  sbind(port, sockfd);
+  slisten(sockfd, BACKLOG);
+  return sockfd;
+}
+
+int main(int argc, char *argv[])
+{
+  ssigaction(SIGINT, endServerHandler);
+
+
+  int port, nbThreads, nbBytes;
+  for(int i=0; i<argc; i++) {
+    if(strcmp(argv[i], "-j") == 0) {
+      nbThreads = atoi(argv[i+1]);
+    } 
+    if(strcmp(argv[i], "-s") == 0) {
+      nbBytes = atoi(argv[i+1]);
+    }
+    if(strcmp(argv[i], "-p") == 0) {
+      port = atoi(argv[i+1]);
+    }
+  }
+  printf("port: %i, number of threads : %i, number of bytes: %i\n", port, nbThreads, nbBytes);
+
+  int sockfd = initSocketServer(port);
+  printf("Server is listening on port %i ...\n", port);
+
+  int integer;
+  while(!end) {
+    int newsockfd = saccept(sockfd);
+    if(newsockfd) {
+      perror("accept failure");
+      return EXIT_FAILURE;
+    }
+
+    int nbRead = sread(newsockfd, &integer, sizeof(int));
+    while(nbRead > 0) {
+      printf("Integer received: %i\n", integer);
+
+      nbRead = sread(newsockfd, &integer, sizeof(int));
+    }
+  }
+
+  sclose(sockfd);
   return 0;
 }
