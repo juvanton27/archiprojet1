4b7dde89f0d53ca17bbe10cf98cf7a3c0bc3ee03
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2022-10-12 22:16:32 +0200

    fix & add - comments

--- client.c
+++ client.c
@@ -3,73 +3,92 @@
 #define BUFFER_SIZE 10000
 #define LOCAL_HOST "127.0.0.1"
 
+/***************************************
+ *              GLOBALS
+***************************************/
 
 volatile sig_atomic_t end = 0;
-
 int sockfd;
 
-void endClientHandler(int sig) {
-    printf("[+] Stoping client ...\n");
-    end = 1;
-}
+/***************************************
+ *              SERVER
+***************************************/
 
+void endClientHandler(int sig)
+{
+  printf("[+] Stoping client ...\n");
+  end = 1;
+}
 
 // SOCKET HANDLER
-int initSocketClient(char* localhost, int port){
-    int socketfd = ssocket();
-    printf("[+] Server socket created successfully.\n");
-    sconnect(localhost, port, socketfd);
-    printf("[+] Connected to Server.\n");
-    return socketfd;
+int initSocketClient(char *localhost, int port)
+{
+  int socketfd = ssocket();
+  printf("[+] Server socket created successfully.\n");
+  sconnect(localhost, port, socketfd);
+  printf("[+] Connected to Server.\n");
+  return socketfd;
 }
 
-uint8_t * generateRequest(uint32_t index, uint32_t size) {
-    printf("[+] Generating request ...\n");
-    // 4 bytes : index, 4 bytes : size of the key, N*N bytes : key
-    uint8_t * request = (uint8_t *) malloc(sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t)*size*size));
-    *(uint32_t *)request = index;
-    *(uint32_t *) (request+4) = size;
-    for(int i=8; i<(size*size)+8; i++) {
-        request[i] = i-8;
+uint8_t *generateRequest(uint32_t index, uint32_t size)
+{
+  uint8_t *request = (uint8_t *)malloc(sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t) * size * size));
+  *(uint32_t *)request = index;
+  *(uint32_t *)(request + 4) = size;
+  for (int i = 8; i < (size * size) + 8; i++)
+  {
+    request[i] = i - 8;
+  }
+  return request;
+}
+
+int main(int argc, char **argv)
+{
+  // Add Ctrl+C handling
+  ssigaction(SIGINT, endClientHandler);
+
+  // Handle the args
+  char *server;
+  int port;
+  int keyBytes = atoi(argv[2]);
+  int req = atoi(argv[4]);
+  int sec = atoi(argv[6]);
+  char *line = argv[7];
+  char *search = ":";
+  server = strtok(line, search);
+  port = atoi(strtok(NULL, search));
+  printf("[+] Launched with options ... size of the key : %d  -  requests/second : %d  -  time of execution (in seconds) : %d  -  ip address : %s -   tcp port : %d \n", keyBytes, req, sec, server, port);
+
+  // Initiate the client
+  sockfd = initSocketClient(server, port);
+
+  // Send requests to the server
+  int s = 0;
+  while (s < sec && !end)
+  {
+    int i = 0;
+    while (i < req)
+    {
+      // Generate request
+      size_t requestSize = sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t) * keyBytes * keyBytes);
+      uint8_t * request = generateRequest(i, keyBytes);
+      swrite(sockfd, &(*request), requestSize);
+
+      // Retreive informations from the response
+      // TO DO : Don't know the size of the encrypted file so have to take bigger but how much
+      size_t responseSize = sizeof(uint8_t) + sizeof(uint32_t) + keyBytes*keyBytes*keyBytes*keyBytes*sizeof(uint8_t);
+      uint8_t * response = malloc(responseSize);
+      sread(sockfd, &(*response), responseSize);
+      uint8_t errorCode = *(uint8_t *)response;
+      uint32_t fileSize = *(uint32_t *)(response+1);
+      uint8_t * encryptedFile = (uint8_t *) malloc(fileSize*sizeof(uint8_t));
+      encryptedFile = (uint8_t *)(response+5);
+      printf("Error code : %i, size : %i, encrypted file : ", errorCode, fileSize);
+      display(encryptedFile, fileSize);
+      i++;
     }
-    return request;
+    sleep(1);
+    s++;
+  }
+  return EXIT_SUCCESS;
 }
-
-int main(int argc, char **argv) {
-    ssigaction(SIGINT, endClientHandler);
-
-    char* server;
-    int port;
-    int keyBytes = atoi(argv[2]);
-    int req = atoi(argv[4]);
-    int sec = atoi(argv[6]);
-    char* line = argv[7];
-    char *search = ":";
-
-    // server will point to 127.0.0.1
-    server = strtok(line, search);
- 
-    // port will point to 2241 for example.
-    port = atoi(strtok(NULL, search));
-
-    printf("KEY SIZE : %d  -  REQUESTS/SECONDS : %d  -  SECONDS : %d  -  SERVER : %s -   TCP PORT : %d \n", keyBytes, req, sec, server,port);
-
-    // Socket
-    sockfd = initSocketClient(server, port);
-
-    int s = 0; //seconds
-    while(s < sec && !end){
-        int i = 0;
-        while(i < req){
-            // Generate request
-            uint8_t * request = generateRequest(i, keyBytes);
-            swrite(sockfd, &(*request), sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t)*keyBytes*keyBytes));
-            i++;
-        }
-        sleep(1);
-        s++;
-    }
-    return EXIT_SUCCESS;
-}
-
-
--- server.c
+++ server.c
@@ -2,8 +2,17 @@
 
 #define NB_FILES 1000
 
+/***************************************
+ *              GLOBALS
+***************************************/
+
 volatile sig_atomic_t end;
 volatile uint8_t **files[NB_FILES];
+
+
+/***************************************
+ *              SERVER
+***************************************/
 
 void endServerHandler(int sig)
 {
@@ -19,6 +28,10 @@ int initSocketServer(int port, int nbThreads)
   printf("[+] Server is listening on port %i ...\n", port);
   return sockfd;
 }
+
+/***************************************
+ *              FILES
+***************************************/
 
 void initFiles(int size)
 {
@@ -39,13 +52,7 @@ void initFiles(int size)
 
 uint8_t ** encryptFile(volatile uint8_t **file, int fileSize, uint8_t **key, int keySize)
 {
-  int size = fileSize / keySize;
-  printf("%i\n", size);
-  printf("File\n");
-  displayAsMatrix((void **) file, fileSize);
-  printf("Key\n");
-  displayAsMatrix((void **) key, keySize);
-  uint8_t ** result = (uint8_t ** )malloc(fileSize * sizeof(uint8_t *));
+  uint8_t ** result = (uint8_t **) malloc(fileSize * sizeof(uint8_t *));
   for(int i=0; i<fileSize; i++) {
     result[i] = (uint8_t *) malloc(fileSize*sizeof(uint8_t));
   }
@@ -54,15 +61,49 @@ uint8_t ** encryptFile(volatile uint8_t **file, int fileSize, uint8_t **key, int
     for (int j = 0; j < fileSize; j++)
     {
       result[i][j] = 0;
-      for (int k = 0; k < keySize; k++)
-      {
+        for (int k = 0; k < keySize; k++)
+        {
         result[i][j] += file[i][k] * key[k][j];
       }
     }
   }
-  printf("Result\n");
-  displayAsMatrix((void **) result, fileSize);
+  // TO DO : Fix encryption
   return result;
+}
+
+
+/***************************************
+ *              MATRIX
+***************************************/
+
+uint8_t ** arrayToMatrix(void * array, int size) {
+  uint8_t * arr = (uint8_t *) array;
+  uint8_t ** matrix = (uint8_t **) malloc(sizeof(uint8_t *)*size);
+  for (int i = 0; i < size; i++)
+  {
+    matrix[i] = (uint8_t *)malloc(sizeof(uint8_t) * size);
+  }
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      matrix[i][j] = arr[(i * size) + j];
+    }
+  }
+  return matrix;
+}
+
+uint8_t * matrixToArray(void ** matrix, int size) {
+  uint8_t ** mat = (uint8_t **) matrix;
+  uint8_t * array = (uint8_t *) malloc(sizeof(uint8_t)*size*size);
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      array[(i*size)+j] = mat[i][j];
+    }
+  }
+  return array;
 }
 
 int main(int argc, char *argv[])
@@ -87,47 +128,47 @@ int main(int argc, char *argv[])
       port = atoi(argv[i + 1]);
     }
   }
-  // printf("PORT : %i, NUMBER OF THREADS : %i, NUMBER OF BYTES : %i\n", port, nbThreads, fileSize);
+  printf("[+] Launched with options ... port : %i, threads used : %i, size of files (in bytes) : %i\n", port, nbThreads, fileSize);
 
-  // Init files
+  // Initiate an array of files
   initFiles(fileSize);
 
-  // Init the server
+  // Initiate the server
   int sockfd = initSocketServer(port, nbThreads);
 
   // Listen to client's requests
-  // 4 bytes : index, 4 bytes : size of the key, N*N bytes : key
   // Don't know the size of the key yet but should be smaller than the size of the file so "sizeof(uint8_t)*fileSize*fileSize"
-  uint8_t *request = (uint8_t *)malloc(sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t) * fileSize * fileSize));
+  size_t requestSize = sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t) * fileSize * fileSize);
+  uint8_t *request = (uint8_t *)malloc(requestSize);
   while (!end)
   {
     int newsockfd = saccept(sockfd);
-    int nbRead = sread(newsockfd, &(*request), sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t) * fileSize * fileSize));
+    int nbRead = sread(newsockfd, &(*request), requestSize);
     while (nbRead > 0)
     {
       // Retreive key informations from the request
-      // uint32_t keyIndex = *(uint32_t *) request;
+      uint32_t fileIndex = *(uint32_t *) request;
       uint32_t keySize = *(uint32_t *)(request + 4);
       uint8_t *key = malloc(sizeof(uint8_t) * keySize);
       key = (uint8_t *)(request + 8);
 
-      // Convert array to matrix
-      uint8_t ** keyMatrix = (uint8_t **) malloc(sizeof(uint8_t *)*keySize);
-      for (int i = 0; i < keySize; i++)
-      {
-        keyMatrix[i] = (uint8_t *)malloc(sizeof(uint8_t) * keySize);
-      }
-      for (int i = 0; i < keySize; i++)
-      {
-        for (int j = 0; j < keySize; j++)
-        {
-          keyMatrix[i][j] = key[(i * keySize) + j];
-        }
-      }
+      uint8_t ** keyMatrix = arrayToMatrix(key, keySize);
+      uint8_t ** encryptedMatrix = encryptFile(files[fileIndex], fileSize, keyMatrix, keySize);
+      uint8_t * encryptedArray = matrixToArray((void **) encryptedMatrix, fileSize);
 
-      encryptFile(files[0], fileSize, keyMatrix, keySize);
+      // Generate response
+      size_t responseSize = sizeof(uint8_t) + sizeof(uint32_t) + fileSize*fileSize*sizeof(uint8_t);
+      uint8_t * response = (uint8_t *) malloc(responseSize);
+      *(uint8_t *)response = 0; // The error code
+      *(uint32_t *)(response+1) = fileSize*fileSize;
+      for (int i = 5; i < (fileSize*fileSize)+5; i++)
+      {
+        response[i] = encryptedArray[i-5];
+      }
+      display(response, responseSize);
+      swrite(newsockfd, *(&response), responseSize);
 
-      nbRead = sread(newsockfd, *(&request), sizeof(uint32_t) + sizeof(u_int32_t) + (sizeof(uint8_t) * fileSize * fileSize));
+      nbRead = sread(newsockfd, *(&request), requestSize);
     }
   }
 
