ec11545b7110226b712222d31cce9cdaa16e661a
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2022-11-14 15:36:59 +0100

    add - creating file

--- client.c
+++ client.c
@@ -3,6 +3,7 @@
 #define BUFFER_SIZE 9
 #define NPAGES 1000
 #define ARRAY_TYPE uint32_t
+#define MAX 1000
 
 /***************************************
  *              GLOBALS
@@ -13,6 +14,8 @@ int port;
 char *server;
 int keysz;
 ARRAY_TYPE *key;
+
+unsigned receive_times[MAX];
 
 int counter = 0;
 
@@ -46,6 +49,9 @@ void *rcv(void *r)
 	// Send key
 	ret = send(sockfd, key, sizeof(ARRAY_TYPE) * keysz * keysz, 0);
 
+	struct timeval start;
+	gettimeofday(&start, NULL);
+
 	unsigned char error;
 	recv(sockfd, &error, 1, 0);
 
@@ -64,6 +70,12 @@ void *rcv(void *r)
 			left -= recv(sockfd, &buffer, b, 0);
 		}
 	}
+
+	unsigned t = (unsigned)(intptr_t)r;
+	struct timeval end;
+	gettimeofday(&end, NULL);
+	receive_times[t] = (end.tv_sec*1000000)+end.tv_usec-(start.tv_sec*1000000)+start.tv_usec;
+
 	sclose(sockfd);
 }
 
@@ -86,6 +98,8 @@ int main(int argc, char **argv)
 		key[i] = i;
 	}
 
+	unsigned sent_times[MAX];
+
 	unsigned long diffrate = 1000000 / rate;
 	int i = 0;
 
@@ -104,11 +118,32 @@ int main(int argc, char **argv)
 			gettimeofday(&end, NULL);
 		}
 
+		sent_times[i] = (end.tv_sec*1000000)+end.tv_usec-(start.tv_sec*1000000)+start.tv_usec;
 		pthread_t thread;
 		pthread_create(&thread, NULL, rcv, (void *)(intptr_t)i);
 		i++;
 		gettimeofday(&end, NULL);
 	}
+
+	int fd1 = sopen("receive_time.log", O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	int fd2 = sopen("send_time.log", O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	char buf[10];
+
+	for(int i=0; i<MAX; i++) {
+		sprintf(buf, "%d;%d", i, receive_times[i]);
+		strcat(buf, "\n");
+		swrite(fd1, buf, strlen(buf));
+		if(i>1) {
+			sprintf(buf, "%d;%d", i, sent_times[i]-sent_times[i-1]);
+		} else {
+			sprintf(buf, "%d;%d", i, sent_times[i]);
+		}
+		strcat(buf, "\n");
+		swrite(fd2, buf, strlen(buf));
+	}
+
+	sclose(fd1);
+	sclose(fd2);
 
 	return EXIT_SUCCESS;
 }
